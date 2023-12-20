36ce3efa5d5b0e3634b1a2dbd032fed65435a9e6
kaple003 <kathrin.pleuger003@stud.fh-dortmund.de>
2023-12-20 22:45:46 +0100

    Kommentare zu den Seiten plan-neu-schritt1, plan-neu-schritt2 und script.js hinzugefügt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
+// Funktion zur Ermittlung der Viewport-Breite
 const getViewportWidth = () =>
   window.innerWidth || document.documentElement.clientWidth;
+/* Gibt innere Breite des Fensters zurück, wenn verfügbar,
+  ansonsten Breite des Dokumentelementes (html-Element) */
 
+// Erstellt Zeichenkette mit der Ausgabe der Viewport-Breite
 const ausgabe = `Die Viewport-Breite beträgt: ${getViewportWidth()} Pixel.`;
+
+// Gibt die Zeichenkette in der Konsole aus
 console.log(ausgabe);
