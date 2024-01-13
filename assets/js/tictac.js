0134bd322c6d7f07af272f17a6f2dd0d356f2172
Kathrin <kathrin.pleuger@icloud.com>
2024-01-13 23:48:24 +0100

    P9 Aufgabe 2 fertig

---
+++ assets/js/tictac.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("TicTac");
+  } else if (i % 3 == 0) {
+    console.log("Tic");
+  } else if (i % 5 == 0) {
+    console.log("Tac");
+  } else {
+    console.log(i);
+  }
+}
